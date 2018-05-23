320615633775c2d667dfd00f0ababbfa7fa0cb1b
Abhishek Gupta <awittlegupta@gmail.com>
2018-05-22 23:24:03 -0700

    first commit

---
+++ 001 Basic Hello World With CLI Arguments/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("What is your name?: ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?: ");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if(hoursOfSleep < 8) {
+                Console.WriteLine("You didn't have enough sleep last night!");
+            } else {
+                Console.WriteLine("You had enough sleep last night!");
+            }
+
+            // Console.WriteLine("Hello, " + name);
+            // Console.WriteLine("Hello, today is " + DateTime.Now.DayOfWeek);
+            // Console.WriteLine("Hello, " + args[0]);
+        }
+    }
+}
---
+++ 002 Variables, Classes, Objects/Classes/Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes {
+    class Program {
+        static void Main(string[] args) {
+            Gradebook entry1 = new Gradebook();
+            entry1.AddGrade(100);
+            entry1.AddGrade(89.5f);
+
+        }
+    }
+}
